c8ffcae63c08d63582632446edef7cc18924adfc
nizarul <faizinnizarul@gmail.com>
2023-03-09 21:58:26 +0700

    tugas 3

---
+++ 3b/3b.php
@@ -0,0 +1,16 @@
+<?php
+
+function urutanAngka($angka) {
+    $mulai = 1;
+    for ($i = 1; $i <= $angka; $i++) {
+        for ($j = 1; $j <= $i; $j++) {
+            echo $mulai . " ";
+            $mulai++;
+        }
+        echo "<br>";
+    }
+}
+
+urutanAngka (5);
+
+?>
